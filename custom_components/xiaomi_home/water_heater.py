bf435374d22130b254e74f5b9bf0d6a0339d5b97
topsworld <sworldtop@gmail.com>
2024-12-17 20:50:01 +0800

    fix: water heater entity add STATE_OFF

--- custom_components/xiaomi_home/water_heater.py
+++ custom_components/xiaomi_home/water_heater.py
@@ -53,6 +53,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.components.water_heater import (
+    STATE_OFF,
     ATTR_TEMPERATURE,
     WaterHeaterEntity,
     WaterHeaterEntityFeature
@@ -116,6 +117,7 @@ class WaterHeater(MIoTServiceEntity, WaterHeaterEntity):
                 if isinstance(prop.value_range, dict):
                     self._attr_min_temp = prop.value_range['min']
                     self._attr_max_temp = prop.value_range['max']
+                    self._attr_precision = prop.value_range['step']
                     if (
                         self._attr_temperature_unit is None
                         and prop.external_unit
@@ -149,6 +151,7 @@ class WaterHeater(MIoTServiceEntity, WaterHeaterEntity):
                 self._attr_supported_features |= (
                     WaterHeaterEntityFeature.OPERATION_MODE)
                 self._prop_mode = prop
+        self._attr_operation_list.append(STATE_OFF)
 
     async def async_turn_on(self) -> None:
         """Turn the water heater on."""
@@ -167,6 +170,12 @@ class WaterHeater(MIoTServiceEntity, WaterHeaterEntity):
         """Set the operation mode of the water heater.
         Must be in the operation_list.
         """
+        if operation_mode == STATE_OFF:
+            await self.set_property_async(prop=self._prop_on, value=False)
+            return
+        if self.get_prop_value(prop=self._prop_on) is False:
+            await self.set_property_async(
+                prop=self._prop_on, value=True, update=False)
         await self.set_property_async(
             prop=self._prop_mode,
             value=self.__get_mode_value(description=operation_mode))
@@ -188,6 +197,8 @@ class WaterHeater(MIoTServiceEntity, WaterHeaterEntity):
     @property
     def current_operation(self) -> Optional[str]:
         """Return the current mode."""
+        if self.get_prop_value(prop=self._prop_on) is False:
+            return STATE_OFF
         return self.__get_mode_description(
             key=self.get_prop_value(prop=self._prop_mode))
 
